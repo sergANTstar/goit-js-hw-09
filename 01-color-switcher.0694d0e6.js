c8031af17c2bc710e457206a066319f47279e2dd
sergANTstar <106014778+sergANTstar@users.noreply.github.com>
2022-08-04 18:12:07 +0000

    Deploying to gh-pages from @ sergANTstar/goit-js-hw-09@1e8118a809fdeea8b10499a8211a7df11b86c044 🚀

--- 01-color-switcher.0694d0e6.js
+++
@@ -1,2 +0,0 @@
-const t={start:document.querySelector("[data-start]"),stop:document.querySelector("[data-stop]"),body:document.querySelector("body")};t.stop.disabled=!0,t.start.addEventListener("click",(function(){interval=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.start.disabled=!0,t.stop.disabled=!1})),t.stop.addEventListener("click",(function(){clearTimeout(interval),t.start.disabled=!1,t.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.0694d0e6.js.map
